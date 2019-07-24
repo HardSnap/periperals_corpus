f98c8e0e398cefc6df13e6ad93ea72ac7a283973
Corteggiani <n.corteggiani@gmail.com>
2019-07-24 11:40:21 +0200

    set tb for aes_scannable (still some mistakes in IP mapped registers configuration)

--- scripts/simu_aes_ctr/tb_scannable_aes_ctr.sv
+++ scripts/simu_aes_ctr/tb_scannable_aes_ctr.sv
@@ -2,7 +2,7 @@
 
 import design_1_axi_vip_0_0_pkg::*;
 import design_1_axi_vip_0_1_pkg::*;
-import design_1_axi_vip_0_2_pkg::*;
+import design_1_axi_vip_2_0_pkg::*;
 import axi_vip_pkg::*;
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -42,7 +42,7 @@ always #1ns aclk = ~aclk;
 
 design_1_axi_vip_0_0_mst_t master_agent_scan_ip;
 design_1_axi_vip_0_1_mst_t master_agent_aes_ctr;
-design_1_axi_vip_0_2_slv_mem_t slv_mem_agent;
+design_1_axi_vip_2_0_slv_mem_t slv_mem_agent;
 
 xil_axi_prot_t  prot = 0;
 xil_axi_resp_t  resp;
@@ -77,45 +77,30 @@ initial begin
 
     // SET KEY
     #10ns
-    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D24, prot, 32'h2b7e1516, resp);
+    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D20, prot, 32'h2b7e1516, resp);
     #10ns
-    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D28, prot, 32'h28aed2a6, resp);
+    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D24, prot, 32'h28aed2a6, resp);
     #10ns
-    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D32, prot, 32'habf71588, resp);
+    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D28, prot, 32'habf71588, resp);
     #10ns
-    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D36, prot, 32'h09cf4f3c, resp);
+    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D32, prot, 32'h09cf4f3c, resp);
+    #10ns
+    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D36, prot, 32'h00000000, resp);
     #10ns
     master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D40, prot, 32'h00000000, resp);
-    #10ns
-    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D44, prot, 32'h00000000, resp);
-    #10ns
-    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D48, prot, 32'h00000000, resp);
-    #10ns
-    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D52, prot, 32'h00000000, resp);
-    #10ns
-    
-    // SET CONFIG
-    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D4, prot, 32'h00000001, resp);
-    #10ns
-    
-    // SET CONTROL -> INIT
-    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D0, prot, 32'h00000001, resp);
-    #10ns
-    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D0, prot, 32'h00000000, resp);
-    #10ns
     
     #100
-    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D56, prot, 32'h6bc1bee2, resp);
+    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D4, prot, 32'h6bc1bee2, resp);
     #10ns
-    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D60, prot, 32'h2e409f96, resp);
+    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D8, prot, 32'h2e409f96, resp);
     #10ns
-    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D64, prot, 32'he93d7e11, resp);
+    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D12, prot, 32'he93d7e11, resp);
     #10ns
-    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D68, prot, 32'h7393172a, resp);
+    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D16, prot, 32'h7393172a, resp);
     #10ns
 
     // SET CONTROL -> NEXT
-    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D0, prot, 32'h00000002, resp);
+    master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D0, prot, 32'h00000001, resp);
     #10ns
     master_agent_aes_ctr.AXI4LITE_WRITE_BURST(AES_BASE+32'D0, prot, 32'h00000000, resp);
     #10ns
